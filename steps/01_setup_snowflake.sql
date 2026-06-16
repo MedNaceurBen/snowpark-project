355865f96c6c53d8901184241bb974f9c0b967a0
Mohamed Naceur BEN HASSINE <mohamed@d765949a-7ab9-45e4-9245-ae140c791fb6.home>
2026-06-16 11:29:12 +0200

    Complete Snowflake setup

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -1,3 +1,77 @@
+
+-- ----------------------------------------------------------------------------
+-- Step #2: Create the account level objects
+-- ----------------------------------------------------------------------------
+
+-- ACCOUNTADMIN : Administration totale du compte Snowflake   
+USE ROLE ACCOUNTADMIN;
+
+-- Roles
+-- Stocke l'utilisateur courant dans une variable
+-- Crée (ou recrée) le rôle HOL_ROLE
+-- Permet à SYSADMIN d'hériter des privilèges de HOL_ROLE
+-- Attribue HOL_ROLE à l'utilisateur courant
 SET MY_USER = CURRENT_USER();
 CREATE OR REPLACE ROLE HOL_ROLE;
 GRANT ROLE HOL_ROLE TO ROLE SYSADMIN;
+GRANT ROLE HOL_ROLE TO USER IDENTIFIER($MY_USER);
+
+
+-- Autorise le rôle à exécuter des Tasks Snowflake
+-- Autorise le rôle à consulter l'état et l'historique des exécutions
+-- Donne accès aux vues système de la base SNOWFLAKE -- (ACCOUNT_USAGE, QUERY_HISTORY, etc.)
+GRANT EXECUTE TASK ON ACCOUNT TO ROLE HOL_ROLE;
+GRANT MONITOR EXECUTION ON ACCOUNT TO ROLE HOL_ROLE;
+GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE TO ROLE HOL_ROLE;
+
+--Databases
+CREATE OR REPLACE DATABASE HOL_DB;
+GRANT OWNERSHIP ON DATABASE HOL_DB TO ROLE HOL_ROLE;
+
+--Warhouses
+CREATE OR REPLACE WAREHOUSE HOL_WH WITH
+    WAREHOUSE_SIZE = 'XSMALL'
+    AUTO_SUSPEND = 300
+    AUTO_RESUME = TRUE;
+
+GRANT OWNERSHIP ON WAREHOUSE HOL_WH TO ROLE HOL_ROLE;
+
+-- ----------------------------------------------------------------------------
+-- Step #3: Create the database level objects
+-- ----------------------------------------------------------------------------
+CREATE OR REPLACE SCHEMA HOL_DB.EXTERNAL;
+CREATE OR REPLACE SCHEMA HOL_DB.RAW_POS;
+CREATE OR REPLACE SCHEMA HOL_DB.HARMONIZED;
+CREATE OR REPLACE SCHEMA HOL_DB.ANALYTICS;
+
+USE SCHEMA HOL_DB.EXTERNAL;
+
+CREATE OR REPLACE FILE FORMAT HOL_DB.EXTERNAL.PARQUET_FORMAT
+    TYPE = 'PARQUET' -- un format de fichier colonnes (columnar)
+    COMPRESSION = 'SNAPPY'; -- méthode de compression;
+
+CREATE OR REPLACE STAGE HOL_DB.EXTERNAL.FROSTBYTE_RAW_STAGE
+    URL = 's3://sfquickstarts/data-engineering-with-snowpark-python/';
+
+
+USE SCHEMA HOL_DB.ANALYTICS;
+
+-- User Defined Function (UDF) : sert à : convertir des pouces (inch) en millimètres (mm)
+-- INCH : nom du paramètre d'entrée de la fonction
+-- NUMBER(35,4)) : type de données du paramètre d'entrée (nombre avec 35 chiffres dont 4 après la virgule)  
+CREATE OR REPLACE FUNCTION HOL_DB.ANALYTICS.INCH_TO_MILLIMETER_UDF(INCH NUMBER(35,4)) 
+RETURNS NUMBER(35,4)
+    AS
+$$
+    inch * 25.4
+$$; 
+
+
+-- This will be added in step 5
+--CREATE OR REPLACE FUNCTION ANALYTICS.FAHRENHEIT_TO_CELSIUS_UDF(TEMP_F NUMBER(35,4))
+--RETURNS NUMBER(35,4)
+--AS
+--$$
+--    (temp_f - 32) * (5/9)
+--$$;
+
